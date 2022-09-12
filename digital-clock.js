d898356d9a7a7258e332eee9bb9ac244c88a3b0f
unknown <nwosu4686@gmail.com>
2022-09-12 16:44:11 +0100

    created 12hrs time function

--- digital-clock.js
+++ digital-clock.js
@@ -1,17 +1,12 @@
-let hours = document.querySelector("#hours");
-let minutes = document.querySelector("#minutes");
-let seconds = document.querySelector("#seconds");
-
-const getTime = () => {
-    let time = new Date();
-    let hoursValue = time.getHours();
-    let minutesValue = time.getMinutes();
-    let secondsValue = time.getSeconds();
-
-    hours.innerHTML = lessThanTen(hoursValue);    
-    minutes.innerHTML = lessThanTen(minutesValue);
-    seconds.innerHTML = lessThanTen(secondsValue);
-}
+let firstClock = document.querySelector(".firstClock");
+let secondClock = document.querySelector(".secondClock");
+let hoursFirstClock = document.querySelector("#hoursFirstClock");
+let minutesFirstClock = document.querySelector("#minutesFirstClock");
+let secondsFirstClock = document.querySelector("#secondsFirstClock");
+let hoursSecondClock = document.querySelector("#hoursSecondClock");
+let minutesSecondClock = document.querySelector("#minutesSecondClock");
+let secondsSecondClock = document.querySelector("#secondsSecondClock");
+let session = document.querySelector("#session");
 
 const lessThanTen = (value) => {
     if (value < 10) {
@@ -22,4 +17,43 @@ const lessThanTen = (value) => {
     }
 }
 
-setInterval(getTime, 0);
+const getTimeFor24Hrs = () => {
+    let time = new Date();
+    let hoursValue = time.getHours();
+    let minutesValue = time.getMinutes();
+    let secondsValue = time.getSeconds();
+
+    hoursFirstClock.innerHTML = lessThanTen(hoursValue);
+    minutesFirstClock.innerHTML = lessThanTen(minutesValue);
+    secondsFirstClock.innerHTML = lessThanTen(secondsValue); 
+}
+
+const getTimeFor12Hrs = () => {
+    let time = new Date();
+    let hoursValue = time.getHours();
+    let minutesValue = time.getMinutes();
+    let secondsValue = time.getSeconds();
+
+    if (hoursValue >= 12) {
+        session.innerHTML = "PM";
+    }
+    else {
+        session.innerHTML = "AM";
+    }
+    
+    if (hoursValue > 12) {
+        hoursValue %= 12;
+        hoursSecondClock.innerHTML = lessThanTen(hoursValue);
+    }
+    else {
+        hoursSecondClock.innerHTML = lessThanTen(hoursValue);
+    }
+    
+    minutesSecondClock.innerHTML = lessThanTen(minutesValue);
+    secondsSecondClock.innerHTML = lessThanTen(secondsValue); 
+
+
+}
+
+setInterval(getTimeFor24Hrs, 0);
+setInterval(getTimeFor12Hrs, 0);
